f43c767fc6b9a29354d48d07ea1d754229f70c74
2karina <karina_kap@hotmail.com>
2023-08-01 13:03:40 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,42 @@
+import validator from './validator.js';
+
+console.log(validator);
+
+// Obter o elemento de entrada do número do cartão pelo seu ID
+const numeroCartaoInput = document.getElementById('numeroCartao');
+
+// Adicionar o evento oninput ao campo de entrada para chamar a função mascararNumeroCartao
+numeroCartaoInput.addEventListener('input', function () {
+  validator.mascararNumeroCartao(this);
+});
+
+//Adicionar o evento onclick ao botão "Validar" para chamar a função validarCartao
+const validarButton = document.querySelector('button[type="submit"]');
+validarButton.addEventListener('click', function (event) {
+  validarCartao(event);
+});
+
+function validarCartao(event) {
+  event.preventDefault();
+
+  const creditCardNumber = numeroCartaoInput.value.replace(/\s/g, ''); // Remover espaços em branco
+  const isValid = validator.isValid(creditCardNumber);
+  const alertElement = document.getElementById('alert');
+  const invalidOptionsElement = document.getElementById('invalidOptions');
+  if (isValid) {
+    alertElement.textContent = 'Número de cartão válido!';
+    alertElement.classList.remove('alert-error');
+    alertElement.classList.add('alert-success');
+    invalidOptionsElement.style.display = 'none';
+  } else {
+    alertElement.textContent = 'Número de cartão inválido! Por favor, verifique o número digitado e tente novamente.';
+    alertElement.classList.remove('alert-success');
+    alertElement.classList.add('alert-error');
+    invalidOptionsElement.style.display = 'block';
+  }
+  alertElement.style.display = 'block';
+  setTimeout(() => {
+    alertElement.style.display = 'none';
+  }, 5000);
+}
+
---
+++ validator.js
@@ -0,0 +1,40 @@
+const validator = {
+  isValid: function (creditCardNumber) {
+    const cleanedNumber = creditCardNumber.replace(/\s/g, '');
+    const digitsArray = Array.from(cleanedNumber).map(Number);
+
+
+    digitsArray.reverse();
+
+    let sum = 0;
+    for (let i = 0; i < digitsArray.length; i++) {
+      if (i % 2 !== 0) {
+        let doubleDigits = digitsArray[i] * 2;
+        if (doubleDigits > 9) {
+          doubleDigits -= 9;
+        }
+        sum += doubleDigits;
+      } else {
+        sum += digitsArray[i];
+      }
+    }
+
+    // Verificar se o resultado da soma é um múltiplo de 10
+    return sum % 10 === 0;
+  },
+  maskify: function (creditCardNumber) {
+    if (creditCardNumber.length <= 4) {
+      return creditCardNumber;
+    }
+    const maskedString = '#'.repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4);
+    return maskedString;
+  },
+  mascararNumeroCartao: function (input) {
+    const creditCardNumber = input.value.replace(/\s/g, ''); // Remover espaços em branco
+    const maskedNumber = this.maskify(creditCardNumber); // Aplicar a função maskify
+    input.value = maskedNumber; // Atualizar o valor do campo de entrada
+  },
+};
+
+
+export default validator;
